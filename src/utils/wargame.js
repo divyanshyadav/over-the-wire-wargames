400e9b26edd863e4e6b8a1a984676b12ef87420e
Divyansh <divyansh.yadav@gmail.com>
2021-09-09 00:43:20 +0530

    solve new level

--- src/utils/wargame.js
+++ src/utils/wargame.js
@@ -1,35 +1,36 @@
-const LocalStorage = require("./local-storage");
-const { exec, shell } = require("./ssh");
+const LocalStorage = require('./local-storage');
+const { exec, shell } = require('./ssh');
 
-function solve({ host, port, levels } = {}) {
+function solve({ name, host, port, levels } = {}) {
     const localStorage = new LocalStorage();
     let promise = Promise.resolve(levels[0].password);
 
-    levels.forEach((level) => {
-        promise = promise.then((flag) => {
+    levels.forEach((level, index) => {
+        const username = `${name}${index}`;
+        promise = promise.then(flag => {
             console.log(level.username, flag, level.cmd);
 
-            if (localStorage.has(level.username)) {
-                return Promise.resolve(localStorage.get(level.username));
+            if (localStorage.has(username)) {
+                return Promise.resolve(localStorage.get(username));
             }
 
             const config = {
                 host,
                 port,
-                username: level.username,
+                username,
                 password: flag,
             };
 
             if (level.shell) return shell(config);
 
-            return exec(level.cmd, config).then((flag) => {
-                localStorage.set(level.username, flag);
+            return exec(level.cmd, config).then(flag => {
+                localStorage.set(username, flag);
                 return Promise.resolve(flag);
             });
         });
     });
 
-    return promise.then((flag) => console.log(flag));
+    return promise.then(flag => console.log(flag));
 }
 
 module.exports = { solve };
--- src/wargames/bandit.js
+++ src/wargames/bandit.js
@@ -1,43 +1,57 @@
 module.exports = {
-    host: "bandit.labs.overthewire.org",
+    name: 'bandit',
+    host: 'bandit.labs.overthewire.org',
     port: 2220,
     levels: [
-        { username: "bandit0", password: "bandit0", cmd: "cat readme" },
-        { username: "bandit1", cmd: "cat /home/bandit1/-" },
-        { username: "bandit2", cmd: "cat spaces\\ in\\ this\\ filename" },
-        { username: "bandit3", cmd: "cat inhere/.hidden" },
-        { username: "bandit4", cmd: "cat /home/bandit4/inhere/-file07" },
+        { password: 'bandit0', cmd: 'cat readme' },
+        { cmd: 'cat /home/bandit1/-' },
+        { cmd: 'cat spaces\\ in\\ this\\ filename' },
+        { cmd: 'cat inhere/.hidden' },
+        { cmd: 'cat /home/bandit4/inhere/-file07' },
         {
-            username: "bandit5",
-            cmd: "find . -type f -size 1033c -exec cat {} \\;",
+            cmd: 'find . -type f -size 1033c -exec cat {} \\;',
         },
         {
-            username: "bandit6",
-            cmd: "find / -type f -user bandit7 -group bandit6 -size 33c -exec cat {} \\; 2>/dev/null",
+            cmd: 'find / -type f -user bandit7 -group bandit6 -size 33c -exec cat {} \\; 2>/dev/null',
         },
         {
-            username: "bandit7",
-            cmd: "cat data.txt | grep millionth | cut -b 11-",
+            cmd: 'cat data.txt | grep millionth | cut -b 11-',
         },
         {
-            username: "bandit8",
-            cmd: "sort data.txt | uniq -u",
+            cmd: 'sort data.txt | uniq -u',
         },
         {
-            username: "bandit9",
             cmd: "strings data.txt | grep '&=' | cut -b 13-",
         },
         {
-            username: "bandit10",
-            cmd: "cat data.txt | base64 -d | cut -b 17-",
+            cmd: 'cat data.txt | base64 -d | cut -b 17-',
         },
         {
-            username: "bandit11",
             cmd: "cat data.txt | tr '[a-zA-Z]' '[n-za-mN-ZA-M]' | cut -b 17-",
         },
         {
-            username: "bandit12",
-            cmd: "cat data.txt | tr '[a-z]' '[n-za-m]' | cut -b 16-",
+            cmd: `
+                temp_dir=$(mktemp -d);
+                cp data.txt $temp_dir;
+                cd $temp_dir;
+                xxd -r data.txt > cmp.gz;
+                gzip -d cmp.gz;
+                mv cmp cmp.bz2;
+                bzip2 -d cmp.bz2;
+                mv cmp cmp.gz;
+                gzip -d cmp.gz;
+                tar -xf cmp;
+                tar -xf data5.bin;
+                mv data6.bin data6.bz2;
+                bzip2 -d data6.bz2;
+                tar -xf data6;
+                mv data8.bin data8.gz;
+                gzip -d data8.gz;
+                cat data8 | cut -b 17-;
+            `,
+        },
+        {
+            cmd: '',
             shell: true,
         },
     ],
